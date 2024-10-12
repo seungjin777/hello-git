787bb9af964a45bd84bb809cf588097e0aef4f8a
Seungjin Kang <ds020309@naver.com>
2024-10-12 07:49:04 +0000

    C2: Second Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World Form Git!");    
+        System.out.println("Changes agter First commit!");
     }
 }
