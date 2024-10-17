db0554e9f8367c00bcd35547034bed37261248c3
Seungjin Kang <ds020309@naver.com>
2024-10-17 16:32:22 +0000

    C4: modify Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World Form Git!");    
         System.out.println("Changes agter First commit!");
+        
+        System.out.println("Changes after Pushing to remote!");    
     }
 }
